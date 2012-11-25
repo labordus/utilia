c4743dbd2841549ac57df7fa870b57910eb7bfdf
Eric McDonald <the.eric.mcdonald@gmail.com>
2012-11-24 23:01:47 -0500

    Update Vim modeline in Python template to handle grim reality of Python 3.

--- src/TEMPLATE.py
+++ src/TEMPLATE.py
@@ -1,19 +1,19 @@
 ###############################################################################
-#				   utilia				      #
+#                                  utilia                                     #
 #-----------------------------------------------------------------------------#
-#									      #
-#   Licensed under the Apache License, Version 2.0 (the "License");	      #
-#   you may not use this file except in compliance with the License.	      #
-#   You may obtain a copy of the License at				      #
-#									      #
-#       http://www.apache.org/licenses/LICENSE-2.0			      #
-#									      #
-#   Unless required by applicable law or agreed to in writing, software	      #
-#   distributed under the License is distributed on an "AS IS" BASIS,	      #
+#                                                                             #
+#   Licensed under the Apache License, Version 2.0 (the "License");           #
+#   you may not use this file except in compliance with the License.          #
+#   You may obtain a copy of the License at                                   #
+#                                                                             #
+#       http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                             #
+#   Unless required by applicable law or agreed to in writing, software       #
+#   distributed under the License is distributed on an "AS IS" BASIS,         #
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
-#   See the License for the specific language governing permissions and	      #
-#   limitations under the License.					      #
-#									      #
+#   See the License for the specific language governing permissions and       #
+#   limitations under the License.                                            #
+#                                                                             #
 ###############################################################################
 
 """
@@ -23,9 +23,9 @@
 
 # Note: Future imports must go before other imports.
 from __future__ import (
-    division		    as __FUTURE_division,
-    absolute_import	    as __FUTURE_absolute_import,
-    print_function	    as __FUTURE_print_function,
+    division                as __FUTURE_division,
+    absolute_import         as __FUTURE_absolute_import,
+    print_function          as __FUTURE_print_function,
 ) # Assumes Python version >= 2.6.
 
 
@@ -33,4 +33,4 @@ __docformat__ = "reStructuredText"
 
 
 ###############################################################################
-# vim: set ft=python sts=4 sw=4 tw=79:					      #
+# vim: set ft=python ts=4 sts=4 sw=4 et tw=79:                                #
